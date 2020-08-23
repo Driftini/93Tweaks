73cffb58caa836d2d8cdff5bfc3978177b14c623
Driftini <yurisilveos741@gmail.com>
2020-08-24 01:15:40 +0200

    ready for release

--- apps/autoclean/autoclean.js
+++ apps/autoclean/autoclean.js
@@ -20,63 +20,65 @@ le._apps["autoclean"] = {
             $log("--save [NAME] | saves clean list in /a/.autoclean/presets/[NAME].json")
             $log("--load [NAME] | loads /a/.autoclean/presets/[NAME].json as preset")
         }
-        function read() {
-            $file.open("/a/.autoclean/clean.json", "String", file => cleanlist = file)
+        function error() {
+            $log.error("Invalid syntax.")
         }
         function write() {
             $db.set(".autoclean/clean.json", cleanlist)
         }
-        function error() {
-            $log.error("Invalid syntax.")
-        }
 
         if (options != undefined) {
-            $log(args)
-            read()
-            if (options.help) {
-                help()
-            } else if (options.a) {
-                if (args != undefined) {
-                    cleanlist.push(args)
+            $file.open("/a/.autoclean/clean.json", "String", (file) => {
+                cleanlist = JSON.parse(file)
+                if (options.help) {
+                    help()
+                } else if (options.a) {
+                    if (args.length > 0) {
+                        cleanlist.push(args)
+                        write()
+                    }
+                    else error()
+                } else if (options.r) {
+                    if (args.length > 0) {
+                        cleanlist = cleanlist.filter(e => e !== args)
+                        write()
+                    }
+                    else error()
+                } else if (options.c) {
+                    cleanlist = []
                     write()
-                }
-                else error()
-            } else if (options.r) {
-                if (args != undefined) {
-                    cleanlist = cleanlist.toString().replace(cleanlist.indexOf(args + ","), ",").split(",")
-                    cleanlist = cleanlist.toString().replace(cleanlist.indexOf(args), ",").split(",")
-                    write()
-                }
-                else error()
-            } else if (options.c) {
-                cleanlist = []
-                write()
-            } else if (options.v) {
-                read()
-                $log(cleanlist)
-            } else if (options.pull) {
-                try {
-                    fetch("https://raw.githack.com/Driftini/93Tweaks/master/apps/autoclean/presets/pullscript.js")
-                        .then(res => res.text()) .then(data => $exe("js " + data))
-                } catch (e) {
-                    $log.error("Pulling failed.")
-                    $log.error(e)
-                }
-            } else if (options.save) {
-                if (args != undefined) {
-                    $file.copy("/a/.autoclean/clean.json", "/a/.autoclean/presets/" + args + ".json", file => {
-                        $file.rename(file, args[0] + ".json")
-                    })
+                } else if (options.v) {
+                    $log(cleanlist)
+                } else if (options.pull) {
+                    try {
+                        fetch("https://raw.githack.com/Driftini/93Tweaks/master/apps/autoclean/presets/pullscript.js")
+                            .then(res => res.text()) .then(data => $exe("js " + data))
+                    } catch (e) {
+                        $log.error("Pulling failed.")
+                        $log.error(e)
+                    }
+                } else if (options.save) {
+                    if (args.length > 0) {
+                        $file.copy("/a/.autoclean/clean.json", "/a/.autoclean/presets/" + args + ".json", file => {
+                            $file.rename(file, args[0] + ".json")
+                        })
+                    } else error()
+                } else if (options.load) {
+                    if (args.length > 0) {
+                        $file.copy("/a/.autoclean/presets/" + args + ".json", "/a/.autoclean/clean.json", file => {
+                            $file.rename(file, "clean.json")
+                        })
+                    } else error()
                 } else error()
-            } else if (options.load) {
-                if (args != undefined) {
-                    $file.copy("/a/.autoclean/presets/" + args + ".json", "/a/.autoclean/clean.json", file => {
-                        $file.rename(file, "clean.json")
-                    })
-                } else error()
-            }
+            })
         } else {
             help()
         }
     }
 }
+
+$file.open("/a/.autoclean/clean.json", "String", (file) => {
+    JSON.parse(file).forEach((value) => {
+        $file.delete(value)
+    })
+})
