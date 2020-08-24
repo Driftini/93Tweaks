0fcfd168cf13400549fd59bb310aa3eb05f8a379
Driftini <34460237+Driftini@users.noreply.github.com>
2020-08-24 12:30:13 +0200

    Update pullscript.js

--- apps/autoclean/presets/pullscript.js
+++ apps/autoclean/presets/pullscript.js
@@ -6,7 +6,7 @@ let done = 0
 
 function pull(value) {
     $log("Pulling " + value + "...")
-    fetch("https://rawcdn.githack.com/Driftini/93Tweaks/f33302294adc360b3bd41fe1a13971638d69fc9c/apps/autoclean/presets/" + value + ".json")
+    fetch("https://raw.githubusercontent.com/Driftini/93Tweaks/master/apps/autoclean/presets/" + value + ".json")
         .then(res => res.json()) .then(data => {$file.save(path + value + ".json", data)})
     done++
     $log(value + " pulled. (" + done + "/" + presets.length + ")")
